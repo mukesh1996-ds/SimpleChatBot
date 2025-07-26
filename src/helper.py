6ae42cb0c874b9d060231372648e8283ffdb512f
mukesh1996-ds <mks.mukesh1996@gmail.com>
2025-07-26 18:09:04 +0530

    main and data updated

--- src/helper.py
+++ src/helper.py
@@ -9,4 +9,14 @@ Keep responses concise but complete.
 Do not hallucinate facts—be honest when unsure.
 """
 
-CUSTOM_SYSTEM_PROMPT="You are an advance assistant that provides translation from English to Hindi"
+# CUSTOM_SYSTEM_PROMPT="You are an advance assistant that provides translation from English to Hindi"
+
+template = """Use the following pieces of information to answer the user's question. 
+If you dont know the answer just say you know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else 
+Helpful answer
+"""
